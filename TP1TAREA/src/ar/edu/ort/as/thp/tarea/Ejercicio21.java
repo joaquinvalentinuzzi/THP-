9206150ad95f4d561ac090b95ae9c17886c587eb
Joaquin Valentinuzzi <98463474+joaquinvalentinuzzi@users.noreply.github.com>
2023-04-10 14:20:30 -0300

    Update Ejercicio21.java

--- TP1TAREA/src/ar/edu/ort/as/thp/tarea/Ejercicio21.java
+++ TP1TAREA/src/ar/edu/ort/as/thp/tarea/Ejercicio21.java
@@ -1,5 +1,12 @@
 package ar.edu.ort.as.thp.tarea;
 
+/*
+21. Realizá un programa que permita al usuario ingresar un número entero entre 1 y 7.
+Debe mostrarse por pantalla el nombre del día de la semana que representa tal número tomando como el primer día de la
+semana el Domingo.
+De ingresar un número fuera de rango debe mostrar error. 
+*/
+
 import java.util.Scanner;
 
 public class Ejercicio21 {
