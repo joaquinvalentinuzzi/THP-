047e2b731ee9ce662bf7b65d4aac9c405d48582e
Joaquin Valentinuzzi <98463474+joaquinvalentinuzzi@users.noreply.github.com>
2023-04-10 13:59:16 -0300

    Update Ejercicio15.java

--- TP1TAREA/src/ar/edu/ort/as/thp/tarea/Ejercicio15.java
+++ TP1TAREA/src/ar/edu/ort/as/thp/tarea/Ejercicio15.java
@@ -1,5 +1,17 @@
 package ar.edu.ort.as.thp.tarea;
 
+/*
+15. Para entrar a la montaña rusa Miedo a las alturas, algo más chica y tranquila que la anterior,
+alcanza con que se cumpla solamente una de las siguientes condiciones: ser mayor de 6 años o medir más de 1,50 metros. 
+Realizá el mismo procedimiento que con el ejercicio anterior pero con los nuevos requisitos. [EC] 
+Nombre Edad Altura  ¿Entra al juego? (V/F) 
+Juan     5   1.45
+María    7   1.23
+Luis     8   1.51
+Ana      9   1.39
+
+*/
+
 import java.util.Scanner;
 
 public class Ejercicio15 {
