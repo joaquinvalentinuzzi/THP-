0a433d726c7367c29e188f256be1f6c642cf9dcc
Joaquin Valentinuzzi <98463474+joaquinvalentinuzzi@users.noreply.github.com>
2023-04-10 14:19:44 -0300

    Update Ejercicio20.java

--- TP1TAREA/src/ar/edu/ort/as/thp/tarea/Ejercicio20.java
+++ TP1TAREA/src/ar/edu/ort/as/thp/tarea/Ejercicio20.java
@@ -1,5 +1,11 @@
 package ar.edu.ort.as.thp.tarea;
 
+/*
+20. Realizá un programa que permita ingresar dos números enteros y la operación a realizar ('+', '-', '*', '/').
+Debe mostrarse el resultado para la operación ingresada. 
+Considerar que no se puede dividir por cero (en ese caso mostrar el texto 'ERROR').
+*/
+
 import java.util.Scanner;
 
 public class Ejercicio20 {
