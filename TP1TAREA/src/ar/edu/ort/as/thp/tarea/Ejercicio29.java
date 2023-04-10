53e9e6ce2c689fcabb5350bb8147598383a814de
Joaquin Valentinuzzi <98463474+joaquinvalentinuzzi@users.noreply.github.com>
2023-04-10 14:33:52 -0300

    Update Ejercicio29.java

--- TP1TAREA/src/ar/edu/ort/as/thp/tarea/Ejercicio29.java
+++ TP1TAREA/src/ar/edu/ort/as/thp/tarea/Ejercicio29.java
@@ -1,5 +1,10 @@
 package ar.edu.ort.as.thp.tarea;
 
+/*
+29. Realizá un programa que permita validar la nota de un examen. Se espera que la nota que el usuario ingrese sea un número comprendido entre 0 y 10. 
+La misma debe ser ingresada tantas veces como sea necesario hasta que quede comprendida dentro del rango indicado.
+*/
+
 import java.util.Scanner;
 
 public class Ejercicio29 {
