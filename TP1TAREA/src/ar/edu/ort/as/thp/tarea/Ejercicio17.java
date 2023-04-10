658d67ff83aa679f307d72d18a541421c37af75e
Joaquin Valentinuzzi <98463474+joaquinvalentinuzzi@users.noreply.github.com>
2023-04-10 14:13:07 -0300

    Update Ejercicio17.java

--- TP1TAREA/src/ar/edu/ort/as/thp/tarea/Ejercicio17.java
+++ TP1TAREA/src/ar/edu/ort/as/thp/tarea/Ejercicio17.java
@@ -1,5 +1,11 @@
 package ar.edu.ort.as.thp.tarea;
-
+/*
+17. Realizá un programa que permita ingresar una edad (entre 1 y 120 años) y un género
+('F' para mujeres, 'M' para hombres). En caso de haber ingresado valores erróneos (edad fuera de rango o género inválido),
+informar tal situación y terminar el programa. Si los datos están bien ingresados el programa debe indicar, sabiendo que las mujeres
+se jubilan con 60 años o más y los hombres con 65 años o más, 
+si la persona está en edad de jubilarse
+*/
 import java.util.Scanner;
 
 public class Ejercicio17 {
