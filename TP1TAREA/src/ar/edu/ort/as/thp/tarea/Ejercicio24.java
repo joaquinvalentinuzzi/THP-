20dd788ed448058dc45cabb1412da522549ed991
Joaquin Valentinuzzi <98463474+joaquinvalentinuzzi@users.noreply.github.com>
2023-04-10 14:24:07 -0300

    Update Ejercicio24.java

--- TP1TAREA/src/ar/edu/ort/as/thp/tarea/Ejercicio24.java
+++ TP1TAREA/src/ar/edu/ort/as/thp/tarea/Ejercicio24.java
@@ -1,4 +1,8 @@
 package ar.edu.ort.as.thp.tarea;
+/*24. Realizá un programa que permita al usuario ingresar dos números enteros num1 y num2, donde el primero siempre deberá ser menor o igual al segundo. La computadora debe mostrar la secuencia de números existentes entre ambos: 
+a. Incluyéndolos; 
+b. Excluyéndolos*/
+
 
 import java.util.Iterator;
 import java.util.Scanner;
