2035ecd825048963b4bfb44da5eccecd0a5c671d
Joaquin Valentinuzzi <98463474+joaquinvalentinuzzi@users.noreply.github.com>
2023-04-10 14:35:30 -0300

    Update Ejercicio30.java

--- TP1TAREA/src/ar/edu/ort/as/thp/tarea/Ejercicio30.java
+++ TP1TAREA/src/ar/edu/ort/as/thp/tarea/Ejercicio30.java
@@ -1,5 +1,12 @@
 package ar.edu.ort.as.thp.tarea;
 
+/*
+30. Realizá un programa que permita realizar varias operaciones matemáticas ingresando un caracter por cada una la operación a realizar ('+',','*', 'F') y
+dos números enteros en el caso que no haya elegido 'F. La computadora debe mostrar el resultado para la operación ingresada. Considerar que no se puede dividir 
+por cero. 
+Cuando la operación ingresada sea 'F' nos indicará que no se desean calcular más operaciones.
+*/
+
 import java.util.Scanner;
 
 public class Ejercicio30 {
