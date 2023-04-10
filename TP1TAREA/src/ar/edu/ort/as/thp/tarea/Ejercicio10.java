6f6f95e7d071517f8b3db358e6973ed260ce11a9
Joaquin Valentinuzzi <98463474+joaquinvalentinuzzi@users.noreply.github.com>
2023-04-10 13:53:15 -0300

    Update Ejercicio10.java

--- TP1TAREA/src/ar/edu/ort/as/thp/tarea/Ejercicio10.java
+++ TP1TAREA/src/ar/edu/ort/as/thp/tarea/Ejercicio10.java
@@ -1,5 +1,10 @@
 package ar.edu.ort.as.thp.tarea;
 
+/*10. Realizá un programa que permita resolver el siguiente problema: Tres personas aportan diferente capital a una
+sociedad y desean saber el valor total aportado y qué porcentaje aportó cada una (indicando nombre y porcentaje). 
+Solicitar la carga por teclado del nombre de cada socio, su capital aportado y a partir de esto calcular e informar
+lo requerido previamente. */
+
 import java.util.Scanner;
 
 public class Ejercicio10 {
