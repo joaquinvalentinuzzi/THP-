f3570108a9beac88c4d8dce015a4cc2846d9ece9
Joaquin Valentinuzzi <98463474+joaquinvalentinuzzi@users.noreply.github.com>
2023-04-10 14:33:04 -0300

    Update Ejercicio28.java

--- TP1TAREA/src/ar/edu/ort/as/thp/tarea/Ejercicio28.java
+++ TP1TAREA/src/ar/edu/ort/as/thp/tarea/Ejercicio28.java
@@ -1,5 +1,10 @@
 package ar.edu.ort.as.thp.tarea;
 
+/*
+28. Realizá un programa que a partir de un número entero cant ingresado por el usuario permita cargar por teclado cant números enteros.
+La computadora debe mostrar cuál fue el mayor número y en qué posición apareció.
+*/
+
 import java.util.Scanner;
 
 public class Ejercicio28 {
