b7758489c1a1a9cbaa5613bd026d4fcd56af17eb
Joaquin Valentinuzzi <98463474+joaquinvalentinuzzi@users.noreply.github.com>
2023-04-10 13:47:15 -0300

    Update Ejercicio4.java

--- TP1TAREA/src/ar/edu/ort/as/thp/tarea/Ejercicio4.java
+++ TP1TAREA/src/ar/edu/ort/as/thp/tarea/Ejercicio4.java
@@ -1,4 +1,7 @@
 package ar.edu.ort.as.thp.tarea;
+/*4. Realizá un programa que permita ingresar el valor monetario de una hora de trabajo y la cantidad de horas trabajadas 
+por día por un trabajador. Debes mostrar el valor del salario semanal, sabiendo que trabaja todos los días hábiles y 
+la mitad de las horas del día hábil los sábados. (Todas las horas valen lo mismo.) */
 
 import java.util.Scanner;
 
