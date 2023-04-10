2b38069fcf67f0f28ed6747e3f4b316de364425f
Joaquin Valentinuzzi <98463474+joaquinvalentinuzzi@users.noreply.github.com>
2023-04-10 13:44:11 -0300

    Update Ejercicio1.java

--- TP1TAREA/src/ar/edu/ort/as/thp/tarea/Ejercicio1.java
+++ TP1TAREA/src/ar/edu/ort/as/thp/tarea/Ejercicio1.java
@@ -1,5 +1,8 @@
 package ar.edu.ort.as.thp.tarea;
 
+/*1. Realizá un programa que permita que el usuario ingrese su nombre. El programa debe emitir 
+una salida con un mensaje de bienvenida que incluya el nombre ingresado. [EC] */
+
 import java.util.Scanner;
 
 public class Ejercicio1 {
