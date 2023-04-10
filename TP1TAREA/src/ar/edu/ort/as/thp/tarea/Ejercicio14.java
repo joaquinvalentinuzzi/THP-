721d060f82ed8f77156db545ed188a5605d28e0a
Joaquin Valentinuzzi <98463474+joaquinvalentinuzzi@users.noreply.github.com>
2023-04-10 13:58:23 -0300

    Update Ejercicio14.java

--- TP1TAREA/src/ar/edu/ort/as/thp/tarea/Ejercicio14.java
+++ TP1TAREA/src/ar/edu/ort/as/thp/tarea/Ejercicio14.java
@@ -1,6 +1,25 @@
 package ar.edu.ort.as.thp.tarea;
 
+
+/*
+14. Para entrar a la montaña rusa Infierno en las alturas se requiere tener al menos 7 años y medir más de 1,50 metros.
+Completá el siguiente cuadro a mano según los requisitos y luego escribí el programa que permita, ejecutándolo cada vez 
+para cada niño y según las edades y estaturas ingresadas por el usuario, obtener los mismos resultados para los siguientes 
+datos: 
+Nombre Edad Altura  ¿Entra al juego? (V/F) 
+Juan     5   1.45
+María    7   1.23
+Luis     8   1.51
+Ana      9   1.39
+ 
+ 
+ 
+
+*/
+
+
 import java.util.Scanner;
+
 
 public class Ejercicio14 {
 	static final Scanner input = new Scanner(System.in);
