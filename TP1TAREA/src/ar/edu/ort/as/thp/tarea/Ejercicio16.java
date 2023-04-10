57746b919fdce668afa2951b35ca670413bb4965
Joaquin Valentinuzzi <98463474+joaquinvalentinuzzi@users.noreply.github.com>
2023-04-10 14:12:20 -0300

    Update Ejercicio16.java

--- TP1TAREA/src/ar/edu/ort/as/thp/tarea/Ejercicio16.java
+++ TP1TAREA/src/ar/edu/ort/as/thp/tarea/Ejercicio16.java
@@ -1,5 +1,7 @@
 package ar.edu.ort.as.thp.tarea;
-
+/*16. Realizá un programa que permita ingresar la cantidad de inscriptos a una conferencia y
+la cantidad de asientos disponibles en el auditorio. Debes indicar si alcanzan los asientos, 
+Si los asientos no alcanzaran indicar cuántos faltan para que todos los inscriptos puedan sentarse*/
 import java.util.Scanner;
 
 public class Ejercicio16 {
