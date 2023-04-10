d1b35d346570a48494c8b167e9d2cded75d9cd75
Joaquin Valentinuzzi <98463474+joaquinvalentinuzzi@users.noreply.github.com>
2023-04-10 13:52:24 -0300

    Update Ejercicio9.java

--- TP1TAREA/src/ar/edu/ort/as/thp/tarea/Ejercicio9.java
+++ TP1TAREA/src/ar/edu/ort/as/thp/tarea/Ejercicio9.java
@@ -1,5 +1,10 @@
 package ar.edu.ort.as.thp.tarea;
 
+/*9. Realizá un programa que permita ingresar dos números que representan las medidas en grados de dos ángulos interiores 
+de cierto triángulo. A partir de los valores de estos dos ángulos el programa debe mostrar el valor en grados del ángulo
+restante. Recordá que la suma de los ángulos interiores de todo triángulo es de 180°. 
+*/
+
 import java.util.Scanner;
 
 public class Ejercicio9 {
