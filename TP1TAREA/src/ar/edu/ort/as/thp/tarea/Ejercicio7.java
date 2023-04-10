5d5eed54a8d231e56281d16822da654e164f7297
Joaquin Valentinuzzi <98463474+joaquinvalentinuzzi@users.noreply.github.com>
2023-04-10 13:50:36 -0300

    Update Ejercicio7.java

--- TP1TAREA/src/ar/edu/ort/as/thp/tarea/Ejercicio7.java
+++ TP1TAREA/src/ar/edu/ort/as/thp/tarea/Ejercicio7.java
@@ -1,7 +1,9 @@
 package ar.edu.ort.as.thp.tarea;
 
 import java.util.Scanner;
-
+/*7. Realizá un programa que permita ingresar el ancho y el largo de un terreno en metros y 
+el valor del metro cuadrado de tierra. Debe mostrarse el valor total del terreno
+y la cantidad de metros de alambre para cercarlo completamente a tres alturas distintas.*/
 public class Ejercicio7 {
 	static final Scanner input = new Scanner(System.in);
 	static final int ALTURAS = 3;
