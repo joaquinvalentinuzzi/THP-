1a2baf9625ebd9463b2a60cf97c7b6d0aa263e7f
Joaquin Valentinuzzi <98463474+joaquinvalentinuzzi@users.noreply.github.com>
2023-04-10 14:14:13 -0300

    Update Ejercicio19UMI.java

--- TP1TAREA/src/ar/edu/ort/as/thp/tarea/Ejercicio19UMI.java
+++ TP1TAREA/src/ar/edu/ort/as/thp/tarea/Ejercicio19UMI.java
@@ -1,4 +1,10 @@
 package ar.edu.ort.as.thp.tarea;
+/*
+18. Realizá un programa que permita al usuario ingresar dos números enteros. 
+La computadora debe indicar si el mayor es divisible por el menor.
+(Un número entero a es divisible por un número entero b cuando el resto de la división entre aybes 0
+*/
+
 
 import java.util.Scanner;
 
